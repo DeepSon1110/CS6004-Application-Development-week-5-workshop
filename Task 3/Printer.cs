cd222fe5967793d861fd733ec5b63164b328b1b8
Dip_San <khadkasandip489@gmail.com>
2025-11-25 21:00:56 +0545

    Task 3

---
+++ Task 3/Printer.cs
@@ -0,0 +1,26 @@
+﻿public class Printer
+{
+    //methods
+    //method to print a message to the console
+    public void print(string message)
+    {
+       Console.WriteLine("message :" + message);
+    }
+
+    //method to Print an integer
+    public void print(int number)
+    {
+       Console.WriteLine("number :" + number);
+    }
+
+    //method to print message multiple times
+    public void print(string message, int count)
+    {
+        for (int i = 0; i < count; i++)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+    
+}
---
+++ Task 3/Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        Printer printer = new Printer();
+
+        //calling method Print(string message)
+        printer.print("Hellosandy");
+
+        //calling method print(int number)
+        printer.print(5);
+
+        //calling method print(string message, int count)
+        printer.print("Hellosandy", 3);
+
+        Console.ReadLine();
+    }
+}
