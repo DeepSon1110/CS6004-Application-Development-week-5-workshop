53f160869625c28e3047b7a0ead797a9d28a59d4
Dip_San <khadkasandip489@gmail.com>
2025-11-25 21:01:23 +0545

    Task 4

---
+++ Task 4/Program.cs
@@ -0,0 +1,23 @@
+﻿public class Program
+{
+    //creating main method
+    public static void Main(string[] args)
+    {
+        //creting object of derived classes
+        //creating object of car class
+        Car myCar = new Car();
+        myCar.StartEngine();
+        myCar.StopEngine();
+        myCar.Display();
+
+        Console.WriteLine();
+
+        //creating object of bike class
+        Bike myBike = new Bike();
+        myBike.StartEngine();
+        myBike.StopEngine();
+        myBike.Display();
+
+
+    }
+}
---
+++ Task 4/Vehicle.cs
@@ -0,0 +1,12 @@
+﻿public abstract class Vehicle
+{
+    public abstract void StartEngine();
+    public abstract void StopEngine();
+
+    public void Display()
+    {
+        Console.WriteLine("This is a vehicle");
+    }
+
+
+}
