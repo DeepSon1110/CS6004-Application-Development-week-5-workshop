30f4711ec5590ad6bdaa62147bab2a5df0a4c122
Dip_San <khadkasandip489@gmail.com>
2025-11-25 09:35:25 +0545

    Task 1

---
+++ Task 1/BankAccount.cs
@@ -0,0 +1,63 @@
+﻿public class BankAccount
+{
+    //private fields
+    private string accountNumber;
+    private double balance;
+
+    //public properties
+    public string AccountNumber
+    {
+        get { return accountNumber; }
+    }
+
+    public double Balance
+    {
+        get { return balance; }
+        private set
+        {
+            if (value > 0)
+                balance = value;
+            else
+                Console.WriteLine("Balance cannot be negative.");
+        }
+    }
+
+    //method to deposit money
+    public void Deposit(double amount)
+    {
+        if (amount > 0)
+        {
+            Balance += amount;
+            Console.WriteLine($"Deposited: {amount}. New Balance: {Balance}");
+        }
+        else
+        {
+            Console.WriteLine("Deposit amount must be positive.");
+        }
+    }
+
+    //method to withdraw money
+    public void Withdraw(double amount)
+    {
+        if (amount > 0 && amount <= Balance)
+        {
+            Balance -= amount;
+            Console.WriteLine($"Withdrew: {amount}. New Balance: {Balance}");
+        }
+        else
+        {
+            Console.WriteLine("Insufficient funds or invalid withdrawal amount.");
+        }
+    }
+
+    //constructor
+    public BankAccount(string accountNumber, double initialBalance)
+    {
+        this.accountNumber = accountNumber;
+        Balance = initialBalance;
+    }
+
+
+
+}
+
---
+++ Task 1/Program.cs
@@ -0,0 +1,20 @@
+﻿class Program
+{
+    static void Main(String[] args)
+    {
+        //creating object of Bank Account Class
+        BankAccount account = new BankAccount("123456789",50000);
+
+        Console.WriteLine("Account Number: " + account.AccountNumber);
+        Console.WriteLine("Initial Balance: " + account.Balance);
+
+        //depositing amount
+        account.Deposit(15000);
+
+        //withdrawing amount
+        account.Withdraw(20000);
+
+        //displaying final balance
+        Console.WriteLine("Final Balance: " + account.Balance);
+    }
+}
